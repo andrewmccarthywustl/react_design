5e61c1fdfa1892829301abf68e7260c2160ba0a1
andrewmccarthywustl <a.g.mccarthy@wustl.edu>
2025-02-25 13:38:15 -0600

    maybe it was this

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react_design/",
-  define: {
-    "process.env": {},
-  },
-  server: {
-    host: "0.0.0.0",
-  },
 });
